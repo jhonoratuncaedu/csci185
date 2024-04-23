047d4f4ad426de6d122abaaeead7455f875aedc6
jhonoratuncaedu <jhonorat@unca.edu>
2024-04-23 12:23:39 -0400

    tut11

---
+++ tutorials/tutorial11/exercise01/main.js
@@ -0,0 +1,20 @@
+function makeRed() {
+    console.log("Change the box's background color to red");
+    document.querySelector("#box").style.backgroundColor = "red";
+}
+
+function makeBlue() {
+    console.log('Change the box\'s background color to blue');
+    document.querySelector("#box").style.backgroundColor = "blue";
+}
+
+function makePink() {
+    console.log('Change the box\'s background color to pink');
+    document.querySelector("#box").style.backgroundColor = "pink";
+}
+
+function makeOrange() {
+    console.log('Change the box\'s background color to orange');
+    document.querySelector("#box").style.backgroundColor = "orange";
+}
+
