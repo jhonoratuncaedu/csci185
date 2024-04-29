38284a7b6d2aeee5c3803b29b512d854f200eb67
jhonoratuncaedu <jhonorat@unca.edu>
2024-04-29 12:15:03 -0400

    quiz2

---
+++ quizes/realquiz/quiz02/exercise01/main.js
@@ -0,0 +1,13 @@
+function changeBackground() {
+document.querySelector("#s1").style.backgroundColor = "blue";
+}
+
+function changeMessage() {
+
+
+}
+
+function showDog() {
+document.querySelector ("#src")
+const el = <img src="images/dog.jpg" />
+}
